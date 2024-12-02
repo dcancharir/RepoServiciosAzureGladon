8159d738444cda1907ce6a3481925776e635e32b
chano64123 <promo201674@gmail.com>
2024-12-02 09:05:26 -0500

    method get ludopatas

--- ServiciosAzureGladcon/ServicioAzureIAS/ServicioController.cs
+++ ServiciosAzureGladcon/ServicioAzureIAS/ServicioController.cs
@@ -1,180 +1,164 @@
 ﻿using ServicioAzureIAS.Clases.GladconData;
+using ServicioAzureIAS.Clases.Mincetur;
 using ServicioAzureIAS.DAL;
+using ServicioAzureIAS.Service;
 using System;
 using System.Collections.Generic;
-using System.Linq;
-using System.Text;
 using System.Threading.Tasks;
 using System.Web.Http;
 
-namespace ServicioAzureIAS
-{
-    public class ServicioController : ApiController
-    {
+namespace ServicioAzureIAS {
+    public class ServicioController : ApiController {
+        private readonly consolidado_deleteDAL consolidado_deleteDAL;
+        private readonly consolidado_tmpDAL consolidado_tmpDAL;
+        private readonly consolidadoDAL consolidadoDAL;
+        private readonly detalle_maquinaDAL detalle_maquinaDAL;
+        private readonly detalle_maquinas_auditDAL detalle_maquinas_auditDAL;
+        private readonly maquinaDAL maquinaDAL;
+        private readonly maquinas_auditDAL maquinas_auditDAL;
+        private readonly salaDAL salaDAL;
+        private readonly MinceturService minceturService;
 
-        consolidado_deleteDAL consolidado_deleteDAL = new consolidado_deleteDAL();
-        consolidado_tmpDAL consolidado_tmpDAL = new consolidado_tmpDAL();
-        consolidadoDAL consolidadoDAL = new consolidadoDAL();
-        detalle_maquinaDAL detalle_maquinaDAL = new detalle_maquinaDAL();
-        detalle_maquinas_auditDAL detalle_maquinas_auditDAL = new detalle_maquinas_auditDAL();
-        maquinaDAL maquinaDAL = new maquinaDAL();
-        maquinas_auditDAL maquinas_auditDAL = new maquinas_auditDAL();
-        salaDAL salaDAL = new salaDAL();
+        public ServicioController() {
+            consolidado_deleteDAL = new consolidado_deleteDAL();
+            consolidado_tmpDAL = new consolidado_tmpDAL();
+            consolidadoDAL = new consolidadoDAL();
+            detalle_maquinaDAL = new detalle_maquinaDAL();
+            detalle_maquinas_auditDAL = new detalle_maquinas_auditDAL();
+            maquinaDAL = new maquinaDAL();
+            maquinas_auditDAL = new maquinas_auditDAL();
+            salaDAL = new salaDAL();
+            minceturService = new MinceturService();
+        }
 
         /// METODOS DEL SERVIDOR
         [HttpPost]
-        public dynamic DevolverDatos()
-        {
+        public dynamic DevolverDatos() {
             return "datos";
         }
 
         [HttpPost]
-        public dynamic DevolverEstado()
-        {
+        public dynamic DevolverEstado() {
             return true;
         }
 
+        [HttpPost]
+        public async Task<IHttpActionResult> ObtenerLudopatasMincetur([FromBody]dynamic obj) {
+            string credencial = obj["credencial"];
+            if(string.IsNullOrEmpty(credencial)) {
+                return BadRequest("No se envío una credencial");
+            }
+            ResponseMinceturLudopata responseMincetur = await minceturService.ObtenerLudopataMincetur(credencial);
+            //ResponseMinceturLudopata responseMincetur = minceturService.ObtenerLudopataMinceturLocal();
+            return Ok(responseMincetur);
+        }
 
         [HttpPost]
-        public dynamic RecepcionarGladconData(dynamic jsonData)
-        {
+        public dynamic RecepcionarGladconData(dynamic jsonData) {
 
             bool recepcionado = true;
 
-            try
-            {
+            try {
 
                 dynamic items = jsonData;
-                if (items.consolidados != null)
-                {
+                if(items.consolidados != null) {
                     List<consolidado> consolidados = items.consolidados.ToObject<List<consolidado>>();
-                    if (consolidados != null)
-                    {
-                        foreach (var item in consolidados)
-                        {
+                    if(consolidados != null) {
+                        foreach(var item in consolidados) {
                             int insertado = consolidadoDAL.InsertarConsolidado(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.consolidados_delete != null)
-                {
+                if(items.consolidados_delete != null) {
                     List<consolidado_delete> consolidados_delete = items.consolidados_delete.ToObject<List<consolidado_delete>>();
-                    if (consolidados_delete != null)
-                    {
-                        foreach (var item in consolidados_delete)
-                        {
+                    if(consolidados_delete != null) {
+                        foreach(var item in consolidados_delete) {
                             int insertado = consolidado_deleteDAL.InsertarConsolidadoDelete(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.consolidados_tmp != null)
-                {
+                if(items.consolidados_tmp != null) {
                     List<consolidado_tmp> consolidados_tmp = items.consolidados_tmp.ToObject<List<consolidado_tmp>>();
-                    if (consolidados_tmp != null)
-                    {
-                        foreach (var item in consolidados_tmp)
-                        {
+                    if(consolidados_tmp != null) {
+                        foreach(var item in consolidados_tmp) {
                             int insertado = consolidado_tmpDAL.InsertarConsolidadoTMP(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.detalles_maquinas != null)
-                {
+                if(items.detalles_maquinas != null) {
                     List<detalle_maquina> detalles_maquinas = items.detalles_maquinas.ToObject<List<detalle_maquina>>();
-                    if (detalles_maquinas != null)
-                    {
-                        foreach (var item in detalles_maquinas)
-                        {
+                    if(detalles_maquinas != null) {
+                        foreach(var item in detalles_maquinas) {
                             int insertado = detalle_maquinaDAL.InsertarDetalleMaquina(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.detalles_maquinas_audit != null)
-                {
+                if(items.detalles_maquinas_audit != null) {
                     List<detalle_maquinas_audit> detalles_maquinas_audit = items.detalles_maquinas_audit.ToObject<List<detalle_maquinas_audit>>();
-                    if (detalles_maquinas_audit != null)
-                    {
-                        foreach (var item in detalles_maquinas_audit)
-                        {
+                    if(detalles_maquinas_audit != null) {
+                        foreach(var item in detalles_maquinas_audit) {
                             int insertado = detalle_maquinas_auditDAL.InsertarDetalleMaquinasAudit(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.maquinas != null)
-                {
+                if(items.maquinas != null) {
                     List<maquina> maquinas = items.maquinas.ToObject<List<maquina>>();
-                    if (maquinas != null)
-                    {
-                        foreach (var item in maquinas)
-                        {
+                    if(maquinas != null) {
+                        foreach(var item in maquinas) {
                             int insertado = maquinaDAL.InsertarMaquina(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.maquinas_audit != null)
-                {
+                if(items.maquinas_audit != null) {
                     List<maquinas_audit> maquinas_audit = items.maquinas_audit.ToObject<List<maquinas_audit>>();
-                    if (maquinas_audit != null)
-                    {
-                        foreach (var item in maquinas_audit)
-                        {
+                    if(maquinas_audit != null) {
+                        foreach(var item in maquinas_audit) {
                             int insertado = maquinas_auditDAL.InsertarMaquinasAudit(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
                         }
                     }
                 }
-                if (items.salas != null)
-                {
+                if(items.salas != null) {
                     List<sala> salas = items.salas.ToObject<List<sala>>();
-                    if (salas != null)
-                    {
-                        foreach (var item in salas)
-                        {
+                    if(salas != null) {
+                        foreach(var item in salas) {
                             int insertado = salaDAL.InsertarSala(item);
 
-                            if (insertado == -1)
-                            {
+                            if(insertado == -1) {
                                 return "Error al insertar en la tabla consolidado";
                             }
 
@@ -182,12 +166,11 @@ namespace ServicioAzureIAS
                     }
                 }
 
-                
 
 
 
-            }catch(Exception ex)
-            {
+
+            } catch(Exception ex) {
                 recepcionado = false;
             }
 
@@ -200,106 +183,74 @@ namespace ServicioAzureIAS
 
         #region
         [HttpGet]
-        public IHttpActionResult ListarConsolidado([FromUri] int Id)
-        {
-            try
-            { 
-                var result= consolidadoDAL.ListarConsolidado(Id);
+        public IHttpActionResult ListarConsolidado([FromUri] int Id) {
+            try {
+                var result = consolidadoDAL.ListarConsolidado(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarConsolidadoDelete([FromUri] int Id)
-        {
-            try
-            {
+        public IHttpActionResult ListarConsolidadoDelete([FromUri] int Id) {
+            try {
                 var result = consolidado_deleteDAL.ListarConsolidadoDelete(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarConsolidadoTmp([FromUri] int Id)
-        {
-            try
-            {
+        public IHttpActionResult ListarConsolidadoTmp([FromUri] int Id) {
+            try {
                 var result = consolidado_tmpDAL.ListarConsolidadoTMP(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarDetalleMaquina([FromUri] int Id)
-        {
-            try
-            {
+        public IHttpActionResult ListarDetalleMaquina([FromUri] int Id) {
+            try {
                 var result = detalle_maquinaDAL.ListarDetalleMaquina(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarDetalleMaquinasAudit([FromUri] int Id)
-        {
-            try
-            {
+        public IHttpActionResult ListarDetalleMaquinasAudit([FromUri] int Id) {
+            try {
                 var result = detalle_maquinas_auditDAL.ListarDetalleMaquinasAudit(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarMaquina([FromUri] int Id)
-        {
-            try
-            {
+        public IHttpActionResult ListarMaquina([FromUri] int Id) {
+            try {
                 var result = maquinaDAL.ListarMaquina(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarMaquinasAudit([FromUri] int Id)
-        {
-            try
-            {
+        public IHttpActionResult ListarMaquinasAudit([FromUri] int Id) {
+            try {
                 var result = maquinas_auditDAL.ListarMaquinasAudit(Id);
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
         [HttpGet]
-        public IHttpActionResult ListarSala()
-        {
-            try
-            {
+        public IHttpActionResult ListarSala() {
+            try {
                 var result = salaDAL.ListarSala();
                 return Ok(result);
-            }
-            catch (Exception ex)
-            {
+            } catch(Exception ex) {
                 return BadRequest(ex.Message);
             }
         }
