85812b4902cbdc4f63cfe1387f0134bdce0864cf
chano <jose.gutierrez@designdevsoftware.com>
2025-06-23 16:44:10 -0500

    fix

--- ServiciosAzureGladcon/ServicioAzureIAS/Jobs/GestionProyectos/SincronizarInformacionBukGestorProyectos.cs
+++ ServiciosAzureGladcon/ServicioAzureIAS/Jobs/GestionProyectos/SincronizarInformacionBukGestorProyectos.cs
@@ -25,167 +25,126 @@ namespace ServicioAzureIAS.Jobs.GestionProyectos {
 
         public async Task Execute(IJobExecutionContext context) {
 
-            //var empresasBD = await empresaDAL.ObtenerEmpresas();
-            //var empresasApi = await gestorBukService.ObtenerEmpresaDesdeApi();
-            //var areasBD = await areaDAL.ObtenerAreas();
-            //var empleadosBD = await empleadoDAL.ObtenerEmpleados();
+            var empresasBD = await empresaDAL.ObtenerEmpresas();
+            var empresasApi = await gestorBukService.ObtenerEmpresaDesdeApi();
+            var areasBD = await areaDAL.ObtenerAreas();
+            var empleadosBD = await empleadoDAL.ObtenerEmpleados();
 
-
-            //#region empresas
-            //funciones.logueo("Empresas:");
-            //foreach(var empresaApi in empresasApi) {
-            //    var empresaLocal = empresasBD.FirstOrDefault(e => e.ID_BUK == empresaApi.ID_BUK);
-            //    Empresa empresa = new Empresa {
-            //        ID_BUK = empresaApi.ID_BUK,
-            //        CO_EMPR = empresaApi.CO_EMPR,
-            //        DE_NOMB = empresaApi.DE_NOMB,
-            //        NU_RUCS = empresaApi.NU_RUCS,
-            //        DE_DIRE = empresaApi.DE_DIRE
-            //    };
-            //    if(empresaLocal != null) {
-            //        if(empresaLocal.isDifferentEmpresaAPi(empresaApi)) {
-            //            funciones.logueo("E Actualizado:");
-            //            funciones.logueo(empresa.ToString());
-            //            await empresaDAL.ActualizarEmpresa(empresa);
-            //        }
-            //    } else {
-            //        funciones.logueo("E insertado:");
-            //        funciones.logueo(empresa.ToString());
-            //        await empresaDAL.InsertarEmpresa(empresa);
-            //    }
-
-
-            //    #region empleados
-
-            //    var empleadosApi = await gestorBukService.ObtenerEmpleadosDesdeApi(empresaApi.ID_BUK);
-
-
-            //    funciones.logueo("Empleados:");
-            //    foreach(var empleadoApi in empleadosApi) {
-            //        empleadoApi.IdEmpresa = empresaApi.ID_BUK;
-
-
-            //        var empleadosLocales = empleadosBD.Where(e => e.IdBuk == empleadoApi.IdBuk).ToList();
-            //        var empleadoEnEmpresaActual = empleadosLocales.FirstOrDefault(e => e.IdEmpresa == empresaApi.ID_BUK);
-            //        Empleado empleado = new Empleado {
-
-            //            TipoDocumento = empleadoApi.TipoDocumento,
-            //            NumeroDocumento = empleadoApi.NumeroDocumento,
-            //            Nombres = empleadoApi.Nombres,
-            //            ApellidoPaterno = empleadoApi.ApellidoPaterno,
-            //            ApellidoMaterno = empleadoApi.ApellidoMaterno,
-            //            NombreCompleto = empleadoApi.NombreCompleto,
-            //            IdEmpresa = empleadoApi.IdEmpresa,
-            //            Empresa = empleadoApi.Empresa,
-            //            Correo = empleadoApi.Correo,
-            //            Celular = empleadoApi.Celular,
-            //            EstadoCese = empleadoApi.EstadoCese,
-            //            IdAreaBuk = empleadoApi.IdAreaBuk,
-            //            Area = empleadoApi.Area,
-            //            IdBuk = empleadoApi.IdBuk,
-
-
-            //        };
-            //        //Area areaDyd = await areaDAL.ObtenerAreaPorIdBuk(empleado.IdAreaBuk);
-            //        var areaDyd = areasBD.FirstOrDefault(a => a.IdAreaBuk == empleadoApi.IdAreaBuk);
-                    
-            //        Area areaNew = new Area();
-            //        if(areaDyd != null) {
-
-            //            areaNew.IdArea = areaDyd.IdArea;
-            //            areaNew.area = empleadoApi.Area;
-            //            areaNew.IdAreaBuk = empleadoApi.IdAreaBuk;
-            //            if(areaNew.IsDifferentAreaApi(areaDyd)) {
-
-            //                await areaDAL.ActualizarArea(areaNew);
-            //            }
-
-            //        } else {
-            //            areaNew.area = empleadoApi.Area;
-            //            areaNew.IdAreaBuk = empleadoApi.IdAreaBuk;
-            //            areaNew.estado = true;
-            //            await areaDAL.InsertarArea(areaNew);
-
-
-            //        }
-
-                
-            //        if(empleadoEnEmpresaActual != null) {
-
-            //            if(empleadoEnEmpresaActual.isDifferentEmpleadoApi(empleadoApi)) {
-            //                empleado.Id = empleadoEnEmpresaActual.Id;
-
-            //                funciones.logueo("Actualizado:");
-            //                funciones.logueo(empleado.ToString());
-            //                await empleadoDAL.ActualizarEmpleado(empleado);
-
-            //            }
-            //        } else {
-            //            funciones.logueo("Insertado:");
-            //            funciones.logueo(empleado.ToString());
-            //            await empleadoDAL.InsertarEmpleado(empleado);
-            //        }
-            //    }
-
-            //    #endregion
-            //}
-            //#endregion
-
-            //#region usuarios
-            //funciones.logueo("fin de area usuario:");
-
-            //var empleadosActualizadosBD = await empleadoDAL.ObtenerEmpleados();
-            //var usuariosBD = await usuarioDAL.ObtenerUsuarios();
-            //var empresasActualizadosBD = await empresaDAL.ObtenerEmpresas();
-            //var areasActualizadosBD = await areaDAL.ObtenerAreas();
-
-            //funciones.logueo("usuario:");
-            //foreach(var usuarioBD in usuariosBD) {
-            //    var empleadoUsuarioLocal = empleadosActualizadosBD.FirstOrDefault(e => e.Id == usuarioBD.EmpleadoId);
-            //    Usuario usuario = new Usuario {
-            //        Nombres = usuarioBD.Nombres,
-            //        Apellidos = usuarioBD.Apellidos,
-            //        Correo = usuarioBD.Correo,
-            //        Telefono = usuarioBD.Telefono,
-            //        EmpresaId = usuarioBD.EmpresaId,
-            //        AreaId = usuarioBD.AreaId,
-            //        Estado = usuarioBD.Estado,
-            //    };
-
-            //    Usuario empleado = new Usuario {
-            //        Nombres = empleadoUsuarioLocal.Nombres,
-            //        Apellidos = string.IsNullOrWhiteSpace(empleadoUsuarioLocal.ApellidoMaterno)
-            //                    ? empleadoUsuarioLocal.ApellidoPaterno
-            //                    : $"{empleadoUsuarioLocal.ApellidoPaterno} {empleadoUsuarioLocal.ApellidoMaterno}",
-            //        Correo = empleadoUsuarioLocal.Correo,
-            //        Telefono = empleadoUsuarioLocal.Celular,
-            //        EmpresaId = empleadoUsuarioLocal.IdEmpresa,
-            //        AreaId = empleadoUsuarioLocal.IdAreaBuk,
-            //        Estado = !empleadoUsuarioLocal.EstadoCese,
-            //    };
-
-
-
-
-            //    if(usuario.isDifferentUsuario(empleado)) {
-            //        empleado.UsuarioId = usuarioBD.UsuarioId;
-
-            //        var empresaLocal = empresasActualizadosBD.FirstOrDefault(e => e.ID_BUK == empleado.EmpresaId);
-            //        var areaLocal = areasActualizadosBD.FirstOrDefault(e => e.IdAreaBuk == empleado.AreaId);
-            //        empleado.EmpresaId = empresaLocal.EmpresaId;
-            //        empleado.AreaId = areaLocal.IdArea;
-            //        await usuarioDAL.ActualizarUsuario(empleado);
-            //        funciones.logueo(empleado.ToString());
-            //        funciones.logueo("Actualizado:");
-
-            //    }
-
-            //}
-            //funciones.logueo("fin de usuario:");
-            //#endregion
 
             #region empresas
+            foreach(var empresaApi in empresasApi) {
+                var empresaLocal = empresasBD.FirstOrDefault(e => e.ID_BUK == empresaApi.ID_BUK);
+                Empresa empresa = new Empresa {
+                    ID_BUK = empresaApi.ID_BUK,
+                    CO_EMPR = empresaApi.CO_EMPR,
+                    DE_NOMB = empresaApi.DE_NOMB,
+                    NU_RUCS = empresaApi.NU_RUCS,
+                    DE_DIRE = empresaApi.DE_DIRE
+                };
+                if(empresaLocal != null) {
+                    if(empresaLocal.isDifferentEmpresaAPi(empresaApi)) {
+                        await empresaDAL.ActualizarEmpresa(empresa);
+                    }
+                } else {
+                    await empresaDAL.InsertarEmpresa(empresa);
+                }
 
+                #region empleados
+                var empleadosApi = await gestorBukService.ObtenerEmpleadosDesdeApi(empresaApi.ID_BUK);
+
+                foreach(var empleadoApi in empleadosApi) {
+                    empleadoApi.IdEmpresa = empresaApi.ID_BUK;
+
+                    var empleadosLocales = empleadosBD.Where(e => e.IdBuk == empleadoApi.IdBuk).ToList();
+                    var empleadoEnEmpresaActual = empleadosLocales.FirstOrDefault(e => e.IdEmpresa == empresaApi.ID_BUK);
+                    Empleado empleado = new Empleado {
+                        TipoDocumento = empleadoApi.TipoDocumento,
+                        NumeroDocumento = empleadoApi.NumeroDocumento,
+                        Nombres = empleadoApi.Nombres,
+                        ApellidoPaterno = empleadoApi.ApellidoPaterno,
+                        ApellidoMaterno = empleadoApi.ApellidoMaterno,
+                        NombreCompleto = empleadoApi.NombreCompleto,
+                        IdEmpresa = empleadoApi.IdEmpresa,
+                        Empresa = empleadoApi.Empresa,
+                        Correo = empleadoApi.Correo,
+                        Celular = empleadoApi.Celular,
+                        EstadoCese = empleadoApi.EstadoCese,
+                        IdAreaBuk = empleadoApi.IdAreaBuk,
+                        Area = empleadoApi.Area,
+                        IdBuk = empleadoApi.IdBuk,
+                    };
+                    var areaDyd = areasBD.FirstOrDefault(a => a.IdAreaBuk == empleadoApi.IdAreaBuk);
+
+                    Area areaNew = new Area();
+                    if(areaDyd != null) {
+                        areaNew.IdArea = areaDyd.IdArea;
+                        areaNew.area = empleadoApi.Area;
+                        areaNew.IdAreaBuk = empleadoApi.IdAreaBuk;
+                        if(areaNew.IsDifferentAreaApi(areaDyd)) {
+                            await areaDAL.ActualizarArea(areaNew);
+                        }
+                    } else {
+                        areaNew.area = empleadoApi.Area;
+                        areaNew.IdAreaBuk = empleadoApi.IdAreaBuk;
+                        areaNew.estado = true;
+                        await areaDAL.InsertarArea(areaNew);
+                    }
+
+                    if(empleadoEnEmpresaActual != null) {
+                        if(empleadoEnEmpresaActual.isDifferentEmpleadoApi(empleadoApi)) {
+                            empleado.Id = empleadoEnEmpresaActual.Id;
+                            await empleadoDAL.ActualizarEmpleado(empleado);
+                        }
+                    } else {
+                        await empleadoDAL.InsertarEmpleado(empleado);
+                    }
+                }
+                #endregion
+            }
+            #endregion
+
+            #region usuarios
+            var empleadosActualizadosBD = await empleadoDAL.ObtenerEmpleados();
+            var usuariosBD = await usuarioDAL.ObtenerUsuarios();
+            var empresasActualizadosBD = await empresaDAL.ObtenerEmpresas();
+            var areasActualizadosBD = await areaDAL.ObtenerAreas();
+
+            foreach(var usuarioBD in usuariosBD) {
+                var empleadoUsuarioLocal = empleadosActualizadosBD.FirstOrDefault(e => e.Id == usuarioBD.EmpleadoId);
+                Usuario usuario = new Usuario {
+                    Nombres = usuarioBD.Nombres,
+                    Apellidos = usuarioBD.Apellidos,
+                    Correo = usuarioBD.Correo,
+                    Telefono = usuarioBD.Telefono,
+                    EmpresaId = usuarioBD.EmpresaId,
+                    AreaId = usuarioBD.AreaId,
+                    Estado = usuarioBD.Estado,
+                };
+
+                Usuario empleado = new Usuario {
+                    Nombres = empleadoUsuarioLocal.Nombres,
+                    Apellidos = string.IsNullOrWhiteSpace(empleadoUsuarioLocal.ApellidoMaterno)
+                                ? empleadoUsuarioLocal.ApellidoPaterno
+                                : $"{empleadoUsuarioLocal.ApellidoPaterno} {empleadoUsuarioLocal.ApellidoMaterno}",
+                    Correo = empleadoUsuarioLocal.Correo,
+                    Telefono = empleadoUsuarioLocal.Celular,
+                    EmpresaId = empleadoUsuarioLocal.IdEmpresa,
+                    AreaId = empleadoUsuarioLocal.IdAreaBuk,
+                    Estado = !empleadoUsuarioLocal.EstadoCese,
+                };
+
+                if(usuario.isDifferentUsuario(empleado)) {
+                    empleado.UsuarioId = usuarioBD.UsuarioId;
+                    var empresaLocal = empresasActualizadosBD.FirstOrDefault(e => e.ID_BUK == empleado.EmpresaId);
+                    var areaLocal = areasActualizadosBD.FirstOrDefault(e => e.IdAreaBuk == empleado.AreaId);
+                    empleado.EmpresaId = empresaLocal.EmpresaId;
+                    empleado.AreaId = areaLocal.IdArea;
+                    await usuarioDAL.ActualizarUsuario(empleado);
+                }
+            }
+            #endregion
+
+            #region empresas
             List<Empresa> empresasGestor = await empresaDAL.ObtenerEmpresas();
             List<Empresa> empresasPro = await empresaDAL.ObtenerEmpresasHolding();
 
@@ -195,24 +154,20 @@ namespace ServicioAzureIAS.Jobs.GestionProyectos {
             List<Area> areasGestor = await areaDAL.ObtenerAreas();
             List<Area> areasPro = await areaDAL.ObtenerAreasHolding();
 
-           
+
             List<Usuario> usuariosPro = await usuarioDAL.ObtenerUsuariosHolding();
 
             funciones.logueo("iniciar copiado de empresas a gestor pro:");
             foreach(var empresaGestor in empresasGestor) {
                 var empresaPro = empresasPro.FirstOrDefault(e => e.ID_BUK == empresaGestor.ID_BUK);
-
                 if(empresaPro == null) {
                     await empresaDAL.InsertarEmpresaHolding(empresaGestor);
-                    funciones.logueo($"Empresa Insertada: {empresaGestor}");
-                } 
+                }
                 if(empresaPro != null && empresaPro.isDifferentEmpresa(empresaGestor)) {
                     empresaGestor.EmpresaId = empresaPro.EmpresaId;
                     await empresaDAL.ActualizarEmpresaHolding(empresaGestor);
-                    funciones.logueo($"Empresa Actualizada: {empresaGestor}");
                 }
             }
-            //funciones.logueo("fin copiado de empresas a gestor pro:");
             #endregion empresas
 
             #region Areas
@@ -223,27 +178,22 @@ namespace ServicioAzureIAS.Jobs.GestionProyectos {
 
                 if(areaPro == null) {
                     await areaDAL.InsertarAreaHolding(areaGestor);
-                    funciones.logueo($"Área Insertada: {areaGestor}");
                 } else if(areaPro.IsDifferentAreaApi(areaGestor)) {
                     areaGestor.IdArea = areaPro.IdArea;
                     await areaDAL.ActualizarAreaHolding(areaGestor);
-                    funciones.logueo($"Área Actualizada: {areaGestor}");
                 }
             }
             #endregion Areas
 
             #region empleados
-
             foreach(var empleadoGestor in empleadosGestor) {
                 var empleadoPro = empleadosPro.FirstOrDefault(e => e.IdBuk == empleadoGestor.IdBuk && e.IdEmpresa == empleadoGestor.IdEmpresa);
 
                 if(empleadoPro == null) {
                     await empleadoDAL.InsertarEmpleadoHolding(empleadoGestor);
-                    funciones.logueo($"Empleado Insertado: {empleadoGestor}");
                 } else if(empleadoPro.isDifferentEmpleado(empleadoGestor)) {
                     empleadoGestor.Id = empleadoPro.Id;
                     await empleadoDAL.ActualizarEmpleadoHolding(empleadoGestor);
-                    funciones.logueo($"Empleado Actualizado: {empleadoGestor}");
                 }
             }
             #endregion empleados
@@ -263,7 +213,6 @@ namespace ServicioAzureIAS.Jobs.GestionProyectos {
                     Estado = usuarioProUpdate.Estado,
                 };
 
-
                 Usuario usuarioActualizado = new Usuario {
                     Nombres = empleadoUsuarioLocal.Nombres,
                     Apellidos = string.IsNullOrWhiteSpace(empleadoUsuarioLocal.ApellidoMaterno)
@@ -276,9 +225,6 @@ namespace ServicioAzureIAS.Jobs.GestionProyectos {
                     Estado = !empleadoUsuarioLocal.EstadoCese
                 };
 
-              
-               
-
                 if(usuario.isDifferentUsuario(usuarioActualizado)) {
                     usuarioActualizado.UsuarioId = usuarioProUpdate.UsuarioId;
 
@@ -290,17 +236,10 @@ namespace ServicioAzureIAS.Jobs.GestionProyectos {
                     usuarioActualizado.AreaId = areaPro.IdArea;
 
                     await usuarioDAL.ActualizarUsuarioHolding(usuarioActualizado);
-                    funciones.logueo("Actualizado:");
-                    funciones.logueo(usuarioActualizado.ToString());
-
                 }
             }
-            // funciones.logueo("fin copiado usuarios a gestor pro:");
-            funciones.logueo("fin del job:");
+            funciones.logueo("fin del job: sincronizar data buk");
             #endregion Usuarios
-
         }
-
-
     }
 }
--- ServiciosAzureGladcon/ServicioAzureIAS/Service/GestorBukService.cs
+++ ServiciosAzureGladcon/ServicioAzureIAS/Service/GestorBukService.cs
@@ -4,7 +4,6 @@ using ServicioAzureIAS.utilitarios;
 using System;
 using System.Collections.Generic;
 using System.Configuration;
-using System.Linq;
 using System.Net;
 using System.Net.Http;
 using System.Text;
@@ -24,11 +23,11 @@ namespace ServicioAzureIAS.Service {
 
         public async Task<List<EmpleadosApi>> ObtenerEmpleadosDesdeApi(long idbuk) {
             List<EmpleadosApi> empleados = new List<EmpleadosApi>();
-           
+
             string url = $"{Uri}/api/EntradaSalida/companies/{idbuk}/employees";
 
             try {
-                
+
                 HttpResponseMessage response = await HttpClient.GetAsync(url);
 
                 string jsonString = await response.Content.ReadAsStringAsync();
@@ -40,26 +39,11 @@ namespace ServicioAzureIAS.Service {
                     sb.AppendLine($"Empresa BUK: {idbuk}");
                     sb.AppendLine($"Display Message: {apiResponse.DisplayMessage}");
                     sb.AppendLine($"Error Messages: {string.Join("\n", apiResponse.ErrorMessage)}");
-                    
-                    funciones.logueo(sb.ToString());
                 }
 
-                //if(response.StatusCode == HttpStatusCode.NotFound) {
-                //    // Registro opcional del 404
-                //    funciones.logueo($"No se encontraron empleados para la empresa ID_BUK: {idbuk} (404)");
-                //    return empleados;
-                //}
-                //if(response.StatusCode == HttpStatusCode.BadRequest) {
-                //    // Registro opcional del 404
-                //    funciones.logueo($"No se encontraron empleados para la empresa ID_BUK: {idbuk} (400)");
-                //    return empleados;
-                //}
                 if(response.StatusCode == HttpStatusCode.OK) {
                     funciones.logueo($"se encontraron: {apiResponse.Data.Count} empleados para la empresa ID_BUK: {idbuk} (200)");
                 }
-
-
-
 
                 if(apiResponse?.Data != null) {
                     empleados = apiResponse.Data;
